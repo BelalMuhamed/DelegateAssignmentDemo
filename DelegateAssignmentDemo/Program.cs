2bb3076d1b88120ce444d8db1eb3c1f2e7f51a8a
BelalBasal <basdibrahim@gmail.com>
2024-08-08 14:38:54 +0300

    V03

--- DelegateAssignmentDemo/Program.cs
+++ DelegateAssignmentDemo/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-           
+            #region V03
+
+            int[] arr = { 7, 6, 5, 8, 2, 3, 9, 10, 55, 4 };
+            SortingArray.BubbleSort(arr, SortingArray.MethodGreater);
+            foreach (int i in arr) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("***********************************************");
+            SortingArray.BubbleSort(arr,SortingArray.MethodLess);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
         }
     }
 }
